d87626e17c8706a0c7c378ab58892b15feb1d919
Andre Stubbe <andrestubbe@proton.me>
2026-05-30 10:12:49 +0200

    Auto Update

--- run-demo.bat
+++ run-demo.bat
@@ -4,7 +4,8 @@ chcp 65001 >nul
 echo ⚡ Building Main Project...
 call mvn -q clean install -DskipTests
 if %ERRORLEVEL% NEQ 0 ( pause & exit /b )
-echo 🚀 Running Hero Demo...
+
+echo 🚀 Running Demo...
 cd examples\Demo
 call mvn -q compile exec:java -Dexec.mainClass=fastdisplay.Demo
 cd ..\..
