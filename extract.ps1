ba4c091d48941b67e9b47f65f1eb38e9cf6085a1
Andre Stubbe <andrestubbe@proton.me>
2026-06-07 13:25:17 +0200

    Auto Update

--- extract.ps1
+++
@@ -1,8 +0,0 @@
-$lines = Get-Content repo.jsonl
-foreach ($l in $lines) {
-    if ($l -match '"native/FastDesktop.cpp"') {
-        $obj = $l | ConvertFrom-Json
-        [IO.File]::WriteAllText("old_FastDesktop.cpp", $obj.content)
-        break
-    }
-}
--- extract_fd.ps1
+++
@@ -1,8 +0,0 @@
-$lines = Get-Content repo.jsonl
-foreach ($l in $lines) {
-    if ($l -match '"native/FastDisplay.cpp"') {
-        $obj = $l | ConvertFrom-Json
-        [IO.File]::WriteAllText("old_FastDisplay.cpp", $obj.content)
-        break
-    }
-}
